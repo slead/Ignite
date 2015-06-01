3aa50bc31a17c4a651703557e7f807bf51a634d1
Stephen Lead <stephen.lead@gmail.com>
2015-06-01 22:43:32 +1000

    Create Devise user model. Configure basic functions in Events controller

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,2 +1,32 @@
 class EventController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :find_event, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @events = Event.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :city, :url)
+  end
+
+  def find_event
+    @event = event.find(params[:id])
+    # @event = event.friendly.find(params[:id])
+  end
+
 end
