2f184b111dcbaccc66706fd7d935709b0ff369ca
Stephen Lead <stephen.lead@gmail.com>
2015-06-02 11:55:24 +1000

    Rename Video_controller Videos_controller

---
+++ app/controllers/videos_controller.rb
@@ -0,0 +1,16 @@
+class VideosController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- test/controllers/videos_controller_test.rb
+++ test/controllers/videos_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class VideoControllerTest < ActionController::TestCase
+class VideosControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
