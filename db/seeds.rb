751df882ed524726a104367b907e1099295523eb
Stephen Lead <stephen.lead@gmail.com>
2015-06-10 16:23:13 +1000

    Add sample videos to the seeds.db file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,147 +7,157 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 events = Event.create!([
-
-{ name:'Ignite Athens',
-  city: 'Athens',
-  country: 'Greece',
-  url:'http://www.igniteathens.gr'
-},
-{ name: 'Ignite Barrie',
-  city: 'Barrie',
-  url:'http://ignitebarrie.com'
-},
-{ name: 'Ignite Bath',
-  city: 'Bath',
-  country: 'United Kingtom',
-  url:'http://2014.bathdigitalfestival.com/event/ignite-for-good'
-},
-{ name: 'Ignite Bergen',
-  city: 'Bergen',
-  country: 'Germany',
-  url:'http://www.ignitebergen.no'
-},
-{ name: 'Ignite Cardiff',
-  city: 'Cardiff',
-  country: 'Wales',
-  url:'http://ignite.wales'
-},
-{ name: 'Ignite Cincinnati',
-  city: 'Cincinnati',
-  country: 'USA',
-  url:'http://ignitecincinnati.net'
-},
-{ name: 'Ignite Colorado Springs',
-  city: 'Colorado Springs',
-  country: 'USA',
-  url:'http://www.ignitecoloradosprings.org'
-},
-{ name: 'Ignite Gotham',
-  city: 'Gotham',
-  country: 'USA',
-  url:'http://ignitegotham.com'
-},
-{ name: 'Ignite Guelph',
-  city: 'Guelph',
-  country: 'The Netherlands',
-  url:'http://igniteguelph.ca'
-},
-{ name: 'Ignite IT Detroit',
-  city: 'Detroit',
-  country: 'USA',
-  url:'http://www.ignite-it.org'
-},
-{ name: 'Ignite Ithica',
-  city: 'Ithica',
-  country: 'USA',
-  url:'http://www.igniteithaca.com'
-},
-{ name: 'Ignite Karachi',
-  city: 'Karachi',
-  country: '',
-  url:'http://www.aku.edu/aboutaku/news/Pages/ignite-II-ccit.aspx'
-},
-{ name: 'Ignite Lansing',
-  city: 'Lansing',
-  country: 'USA',
-  url:'http://www.ignitelansing.com'
-},
-{ name: 'Ignite Lincoln',
-  city: 'Lincoln',
-  country: 'USA',
-  url:'http://ignitelincoln.org'
-},
-{ name: 'Ignite Memphis',
-  city: 'Memphis',
-  country: 'USA',
-  url:'http://ignitememphis.com'
-},
-{ name: 'Ignite Oakland',
-  city: 'Oakland',
-  country: 'USA',
-  url:'http://igniteoakland.com'
-},
-{ name: 'Ignite OKC',
-  city: 'Oklahoma City',
-  country: 'USA',
-  url:'http://igniteokc.com'
-},
-{ name: 'Ignite Phoenix',
-  city: 'Phoenix',
-  country: 'USA',
-  url:'http://www.ignitephoenixafterhours.com'
-},
-{ name: 'Ignite Reno',
-  city: 'Reno',
-  country: 'USA',
-  url:'http://www.ignitereno.com'
-},
-{ name: 'Ignite Salisbury',
-  city: 'Salisbury',
-  country: 'USA',
-  url:'http://ignitesalisbury.com'
-},
-{ name: 'Ignite Salt Lake',
-  city: 'Salt Lake',
-  country: 'USA',
-  url:'http://www.ignitesaltlake.com'
-},  
-{ name: 'Ignite Seattle',
-  city: 'Seattle',
-  country: 'USA',
-  url:'http://igniteseattle.com'
-},
-{ name: 'Ignite Sonoma County',
-  city: 'Sonoma County',
-  country: 'USA',
-  url:'http://www.jccsoco.org/specialevents/ignite-event/?link=IgniteEvent'
-},
-{ name: 'Ignite St John (SJ)',
-  city: 'St John',
-  country: 'Canada',
-  url:'http://www.ignitesj.ca'
-},
-{ name: 'Ignite Steamboat Springs',
-  city: 'Steamboat Springs',
-  country: 'USA',
-  url:'http://ignitesteamboat.com'
-},
-{ name: 'Ignite Tampa Bay',
-  city: 'Tampa Bay',
-  country: 'USA',
-  url:'http://ignitetampa.org'
-},
-{ name: 'Ignite Vancouver',
-  city: 'Vancouver',
-  country: 'Canada',
-  url:'http://ignitebc.com'
-},
-{ name: 'Ignite Sydney',
-  city: 'Sydney',
-  country: 'Australia',
-  url:'http://www.ignitesydney.com'
-}
-
-
-
+  { name:'Ignite Athens',
+    city: 'Athens',
+    country: 'Greece',
+    url:'http://www.igniteathens.gr'
+  },
+  { name: 'Ignite Barrie',
+    city: 'Barrie',
+    url:'http://ignitebarrie.com'
+  },
+  { name: 'Ignite Bath',
+    city: 'Bath',
+    country: 'United Kingtom',
+    url:'http://2014.bathdigitalfestival.com/event/ignite-for-good'
+  },
+  { name: 'Ignite Bergen',
+    city: 'Bergen',
+    country: 'Germany',
+    url:'http://www.ignitebergen.no'
+  },
+  { name: 'Ignite Cardiff',
+    city: 'Cardiff',
+    country: 'Wales',
+    url:'http://ignite.wales'
+  },
+  { name: 'Ignite Cincinnati',
+    city: 'Cincinnati',
+    country: 'USA',
+    url:'http://ignitecincinnati.net'
+  },
+  { name: 'Ignite Colorado Springs',
+    city: 'Colorado Springs',
+    country: 'USA',
+    url:'http://www.ignitecoloradosprings.org'
+  },
+  { name: 'Ignite Gotham',
+    city: 'Gotham',
+    country: 'USA',
+    url:'http://ignitegotham.com'
+  },
+  { name: 'Ignite Guelph',
+    city: 'Guelph',
+    country: 'The Netherlands',
+    url:'http://igniteguelph.ca'
+  },
+  { name: 'Ignite IT Detroit',
+    city: 'Detroit',
+    country: 'USA',
+    url:'http://www.ignite-it.org'
+  },
+  { name: 'Ignite Ithica',
+    city: 'Ithica',
+    country: 'USA',
+    url:'http://www.igniteithaca.com'
+  },
+  { name: 'Ignite Karachi',
+    city: 'Karachi',
+    country: '',
+    url:'http://www.aku.edu/aboutaku/news/Pages/ignite-II-ccit.aspx'
+  },
+  { name: 'Ignite Lansing',
+    city: 'Lansing',
+    country: 'USA',
+    url:'http://www.ignitelansing.com'
+  },
+  { name: 'Ignite Lincoln',
+    city: 'Lincoln',
+    country: 'USA',
+    url:'http://ignitelincoln.org'
+  },
+  { name: 'Ignite Memphis',
+    city: 'Memphis',
+    country: 'USA',
+    url:'http://ignitememphis.com'
+  },
+  { name: 'Ignite Oakland',
+    city: 'Oakland',
+    country: 'USA',
+    url:'http://igniteoakland.com'
+  },
+  { name: 'Ignite OKC',
+    city: 'Oklahoma City',
+    country: 'USA',
+    url:'http://igniteokc.com'
+  },
+  { name: 'Ignite Phoenix',
+    city: 'Phoenix',
+    country: 'USA',
+    url:'http://www.ignitephoenixafterhours.com'
+  },
+  { name: 'Ignite Reno',
+    city: 'Reno',
+    country: 'USA',
+    url:'http://www.ignitereno.com'
+  },
+  { name: 'Ignite Salisbury',
+    city: 'Salisbury',
+    country: 'USA',
+    url:'http://ignitesalisbury.com'
+  },
+  { name: 'Ignite Salt Lake',
+    city: 'Salt Lake',
+    country: 'USA',
+    url:'http://www.ignitesaltlake.com'
+  },  
+  { name: 'Ignite Seattle',
+    city: 'Seattle',
+    country: 'USA',
+    url:'http://igniteseattle.com'
+  },
+  { name: 'Ignite Sonoma County',
+    city: 'Sonoma County',
+    country: 'USA',
+    url:'http://www.jccsoco.org/specialevents/ignite-event/?link=IgniteEvent'
+  },
+  { name: 'Ignite St John (SJ)',
+    city: 'St John',
+    country: 'Canada',
+    url:'http://www.ignitesj.ca'
+  },
+  { name: 'Ignite Steamboat Springs',
+    city: 'Steamboat Springs',
+    country: 'USA',
+    url:'http://ignitesteamboat.com'
+  },
+  { name: 'Ignite Tampa Bay',
+    city: 'Tampa Bay',
+    country: 'USA',
+    url:'http://ignitetampa.org'
+  },
+  { name: 'Ignite Vancouver',
+    city: 'Vancouver',
+    country: 'Canada',
+    url:'http://ignitebc.com'
+  },
+  { name: 'Ignite Sydney',
+    city: 'Sydney',
+    country: 'Australia',
+    url:'http://www.ignitesydney.com'
+  }
 ])
+
+videos = Video.create!([
+  {  
+    id: 3, title: "What if you say Yes to everything?", url: "https://www.youtube.com/watch?v=InYTkTKSBfo", event_id: 22
+  },
+  {
+    id: 1, title: "How to get 5 million people to read your comic", url: "https://www.youtube.com/watch?v=QYyJZOHgpco", event_id: 22
+  },
+  {
+    id: 2, title: "Save the planet, eat a bug", url: "https://www.youtube.com/watch?v=QSs27t8nPlM", event_id: 22
+  }
+])
+
+}
