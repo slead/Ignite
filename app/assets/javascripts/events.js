d35b4effe0f38b162df2b91bf335b45171118d1a
Stephen Lead <stephen.lead@gmail.com>
2015-06-16 15:44:18 +1000

    Add a comma between Ignite name and city on the map page

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -72,7 +72,7 @@ ready = function() {
           event = geojson[idx].properties;
           html += "<div class='col-md-4 col-sm-4 col-xs-4 event_content' id='" + event.id + "''>";
           html += "<div class='name'><h4><a href=" + event.url + ">" + event.name + "<span class='link-spanner'></span></a></h2></div>";
-          html += "<div class='city'><p>" + event.city + "," + event.country + "</h4></div>";
+          html += "<div class='city'><p>" + event.city + ", " + event.country + "</h4></div>";
           html += "</div>"; // event_content
           if (idx % 3 == 2) {
             html += "</div>"
