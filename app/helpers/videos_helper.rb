b3d3595b7f2b0e19a2715df5ab3108531372c708
Stephen Lead <stephen.lead@gmail.com>
2015-06-16 12:11:58 +1000

    Hide the banner on the YouTube iframes

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,6 +1,6 @@
 module VideosHelper
   def embed(youtube_url)
     youtube_id = youtube_url.split("=").last
-    content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}")
+    content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}?hd=1&rel=0&autohide=0&showinfo=0")
   end
 end
