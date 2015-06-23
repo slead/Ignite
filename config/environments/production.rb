516c79b212c506cb9785223b4666da409046ed8f
Stephen Lead <stephen.lead@gmail.com>
2015-06-23 16:16:02 +1000

    Make speaker name mandatory on videos. Configure sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,13 +87,15 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'rocky-sierra-8674.heroku.com' }
+
   ActionMailer::Base.smtp_settings = {
-    :address    => "smtp.sendgrid.net",
-    :port       => 25,
-    :user_name  => ENV['SENDGRID_USERNAME'],
-    :password   => ENV['SENDGRID_PASSWORD'],
-    :domain     => ENV['SENDGRID_DOMAIN'],
-    :authentication  => :plain
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
 
 end
