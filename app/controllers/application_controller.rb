d621267972d91c4a0918108dd5c582127972f487
Stephen Lead <stephen.lead@gmail.com>
2015-06-23 20:00:00 +1000

    Redirect the user more sensibly after forcing them to sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,24 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   # Open the Admin panel after the user signs in
-  def after_sign_in_path_for(resource)
-    admin_path
-  end
+  # def after_sign_in_path_for(resource)
+  #   admin_path
+  # end
+
+  after_filter :store_location
+
+  private
+    def store_location
+      session[:return_to] = request.fullpath
+    end
+
+    def clear_stored_location
+      session[:return_to] = nil
+    end
+
+    def redirect_back_or_to(alternate)
+      redirect_to(session[:return_to] || alternate)
+      clear_stored_location
+    end
+
 end
