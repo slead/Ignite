4b1997cab17dc162d18bc64a614fa3f096f44318
Stephen Lead <stephen.lead@gmail.com>
2015-06-25 15:08:35 +1000

    Change the geocoder to use OpenStreetMap (no daily limit)

---
+++ config/initializers/geocoder.rb
@@ -0,0 +1,14 @@
+Geocoder.configure(
+
+  # geocoding service (see below for supported options):
+  :lookup => :nominatim,
+
+  # geocoding service request timeout, in seconds (default 3):
+  :timeout => 10,
+
+  # set default units to kilometers:
+  :units => :km,
+
+  :http_headers => { "User-Agent" => "stephen.lead+ignitetalks@gmail.com" }
+
+)
