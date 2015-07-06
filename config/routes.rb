d380e73f663e0b90c65e768f7c47280cde8e5c7c
Stephen Lead <stephen.lead@gmail.com>
2015-07-06 20:04:29 +1000

    Add support for markdown in the blog posts

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,14 @@ Rails.application.routes.draw do
   resources :contacts, only: [:new, :create]
   resources :videos
   resources :upcomings
-  resources :posts
+  resources :posts, except: [:index]
 
   get '/about', to: 'content#about', as: :about #=> about_path
   get '/admin', to: 'admin#index', as: :admin #=> admin_path
-  get '/start', to: 'content#start', as: :start #=> start_path
   get '/contact', to: 'contacts#new', as: :contact #=> contact_path
   get '/feedback', to: 'contacts#new', as: :feedback #=> contact_path
+  get '/blog', to: 'posts#index', as: :blog #=> blog_path
+  get '/start', to: 'content#start', as: :start #=> start_path
   get '/:id', to: 'events#show', as: :event_page
 
   root to: "content#home"
