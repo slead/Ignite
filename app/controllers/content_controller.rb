c5b9868156bd24ba4f50ebef66cf3dc3399efd8a
Stephen Lead <stephen.lead@gmail.com>
2015-07-07 11:28:46 +1000

    Only show Published upcoming events on the homepage

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -6,7 +6,7 @@ class ContentController < ApplicationController
   def home
     @first_video = Video.where(status: 'published').last
     @videos = Video.where(status: 'published').offset(1).limit(8).order('created_at DESC')
-    @upcomings = Upcoming.where('date > ?', Date.yesterday).limit(12).order('date ASC')
+    @upcomings = Upcoming.where(status:'published').where('date > ?', Date.yesterday).limit(12).order('date ASC')
   end
 
   def content_params
