3869f8439f9f518b5878710badbc1a4046f668b9
Stephen Lead <stephen.lead@gmail.com>
2015-07-08 10:11:46 +1000

    Restore dataTables functionality to the admin page (was broken)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -61,6 +61,72 @@ function pageLoad() {
     $(".navbar").show();
     $(window).unbind('scroll');
   }
+
+  // Enable datatables on the admin page
+  $('#videos').dataTable({
+    "columnDefs": [
+      { "orderable": false, "targets": [2,6,7,8] }
+    ]
+  });
+
+  $('#draft_videos').dataTable({
+    "columnDefs": [
+      { "orderable": false, "targets": [2,6,7,8] }
+    ]
+  });
+
+  $('#events').DataTable({
+    "columnDefs": [
+      { "orderable": false, "targets": [4,5] }
+    ]
+  });
+
+  $('#draft_events').DataTable({
+    "columnDefs": [
+      { "orderable": false, "targets": [4,5] }
+    ]
+  });
+
+  $('#upcoming').DataTable({
+    "columnDefs": [
+      { "orderable": false, "targets": [3] }
+    ]
+  });
+
+  $('#draft_upcoming').DataTable({
+    "columnDefs": [
+      { "orderable": false, "targets": [3] }
+    ]
+  });
+
+  // Toggle switches for draft/published, using BootStrapSwitch plugin
+  $(".bootstrapSwitch").bootstrapSwitch();
+  if(Cookies.get("chkDrafts") == 'true') {
+    $("#chkDrafts").bootstrapSwitch('state', true)
+    $(".draft").toggle(true);
+  } else {
+    $("#chkDrafts").bootstrapSwitch('state', false)
+    $(".draft").toggle(false);
+  }
+
+  if(Cookies.get("chkPublished") == 'true') {
+    $("#chkPublished").bootstrapSwitch('state', true)
+    $(".published").toggle(true);
+  } else {
+    $("#chkPublished").bootstrapSwitch('state', false)
+    $(".published").toggle(false);
+  }
+
+  $("#chkDrafts").on('switchChange.bootstrapSwitch', function(event, state) {
+    $(".draft").toggle(state);
+    Cookies.set('chkDrafts', state);
+  });
+
+  $("#chkPublished").on('switchChange.bootstrapSwitch', function(event, state) {
+    $(".published").toggle(state);
+    Cookies.set('chkPublished', state);
+  });
+  
 }
 
 $(document).ready(pageLoad);
