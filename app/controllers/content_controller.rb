f7d9f0771514eae7dbe835754fe68b22e97376bf
Stephen Lead <stephen.lead@gmail.com>
2015-07-13 11:55:45 +1000

    Add search box and tag query to the homepage

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -5,8 +5,9 @@ class ContentController < ApplicationController
 
   def home
     @first_video = Video.where(status: 'published').last
-    @videos = Video.where(status: 'published').offset(1).limit(8).order('created_at DESC')
+    @videos = Video.where(status: 'published').offset(1).limit(12).order('created_at DESC')
     @upcomings = Upcoming.where(status:'published').where('date > ?', Date.yesterday).limit(12).order('date ASC')
+    @tags = Tag.where(major: true).order(:name)
   end
 
   def content_params
