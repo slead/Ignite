a7e256674a1a311b71cd9bb0cb906cf5026fca06
Stephen Lead <stephen.lead@gmail.com>
2015-07-17 14:34:14 +1000

    Add a row to separate .col's correctly. Simplify thumbnail creation in the partial

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -5,8 +5,8 @@ module VideosHelper
     content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}?hd=1&rel=0&autohide=0&showinfo=0", :allowfullscreen => "allowfullscreen")
   end
 
-  def thumbnail(video, rating_class)
+  def thumbnail(video, rating_class, max_length)
     # Generate a thumbnail image containing viewing stats, which links to the video
-    render 'videos/thumbnail', :video => video, :rating_class => rating_class
+    render 'videos/thumbnail', :video => video, :rating_class => rating_class, :max_length => max_length
   end
 end
