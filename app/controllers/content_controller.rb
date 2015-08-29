18c3a5aa890cc0d7f9ad4cec2b2cf2f845847c09
Stephen Lead <stephen.lead@gmail.com>
2015-08-29 23:15:24 +1000

    Ensure that the First video doesn't appear in the Featured list

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -7,7 +7,7 @@ class ContentController < ApplicationController
 
   def home
     @first_video = Video.where(status: 'published').where(featured: true).last
-    @videos = Video.where(status: 'published').where(featured: true).offset(1).limit(12).order("RANDOM()")
+    @videos = Video.where(status: 'published').where(featured: true).where.not(id: @first_video.id).limit(12).order("RANDOM()")
     @upcomings = Upcoming.where(status:'published').where('date > ?', Date.yesterday).limit(12).order('date ASC')
     @tags = Tag.where(major: true).order(:name)
   end
