fcb30c175f67262353b8b45476ece9854138d576
Stephen Lead <stephen.lead@gmail.com>
2015-09-13 22:18:40 +1000

    add error handling for when there are no videos loaded yet

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -6,8 +6,14 @@ class ContentController < ApplicationController
   end
 
   def home
-    @first_video = Video.where(status: 'published').where(featured: true).last
-    @videos = Video.where(status: 'published').where(featured: true).where.not(id: @first_video.id).limit(12).order("RANDOM()")
+    begin
+      @first_video = Video.where(status: 'published').where(featured: true).last
+      @videos = Video.where(status: 'published').where(featured: true).where.not(id: @first_video.id).limit(12).order("RANDOM()")
+    rescue
+      puts "need to add some videos"
+      @first_video = nil
+      @videos = []
+    end
     @upcomings = Upcoming.where(status:'published').where('date > ?', Date.yesterday).limit(12).order('date ASC')
     @tags = Tag.where(major: true).order(:name)
   end
