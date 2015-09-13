a24a62e7e9ab7862cb276fc5e179f28b33b5116e
Stephen Lead <stephen.lead@gmail.com>
2015-09-13 19:30:39 +1000

    When creating a new video, retrieve information from the URL
    This method will be used when importing a playlist, with another script
    calling the /videos/new URL repeatedly, with the parameters pre-filled

--- app/assets/javascripts/videos.js
+++ app/assets/javascripts/videos.js
@@ -11,22 +11,20 @@ ready = function() {
     $("#clear-filter").hide();
   }
 
+  //Get the Ignite ID and YouTube ID from the URL, if present. This method will be used when importing a playlist,
+  //with another script calling the /videos/new URL repeatedly, with the parameters pre-filled.
+  uid = getUrlParameter("uid");
+  igniteID = getUrlParameter("igniteid");
+  if(uid != null && igniteID != null) {
+    //If these properties are present, automatically retrieve the information from the YouTube API
+    $("#video_event_id").val(igniteID);
+    $("#video_url").val("https://www.youtube.com/watch?v=" + uid);
+    retrieveYouTubeDetails();
+  }
+
+  //Retrieve the video's details from the YouTube API
   $("#btnYouTubeRetrieve").on("click", function() {
-    $("#btnYouTubeRetrieve").addClass("disabled");
-    url = $("#video_url").val();
-    checkURL = ytVidId(url);
-    $.getJSON( checkURL, function( data ) {
-      if (data.items.length > 0) {
-        title = data.items[0].snippet.title;
-        description = data.items[0].snippet.description;
-        $("#video_title").val(title);
-        $("#video_description").val(description);
-        $(".details").show();
-      } else {
-        alert("Unable to retrieve details from YouTube. Please try another URL")
-        $(".details").hide();
-      }
-    });
+    retrieveYouTubeDetails();
   });
 
   $("#video_url").on("keyup", function() {
@@ -39,18 +37,36 @@ ready = function() {
 
 }
 
-function getUrlParameter(sParam)
-{
-    var sPageURL = window.location.search.substring(1);
-    var sURLVariables = sPageURL.split('&');
-    for (var i = 0; i < sURLVariables.length; i++) 
-    {
-        var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] == sParam) 
-        {
-            return sParameterName[1];
-        }
+function getUrlParameter(sParam){
+  var sPageURL = window.location.search.substring(1);
+  var sURLVariables = sPageURL.split('&');
+  for (var i = 0; i < sURLVariables.length; i++) 
+  {
+      var sParameterName = sURLVariables[i].split('=');
+      if (sParameterName[0] == sParam) 
+      {
+          return sParameterName[1];
+      }
+  }
+}
+
+function retrieveYouTubeDetails(){
+  //Make a call to the YouTube API to retrieve details about this video
+  $("#btnYouTubeRetrieve").addClass("disabled");
+  url = $("#video_url").val();
+  checkURL = ytVidId(url);
+  $.getJSON( checkURL, function( data ) {
+    if (data.items.length > 0) {
+      title = data.items[0].snippet.title;
+      description = data.items[0].snippet.description;
+      $("#video_title").val(title);
+      $("#video_description").val(description);
+      $(".details").show();
+    } else {
+      alert("Unable to retrieve details from YouTube. Please try another URL")
+      $(".details").hide();
     }
+  });
 }
 
 function ytVidId(url) {
