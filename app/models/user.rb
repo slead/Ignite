dcf586fa18cce8dbd23e0406d289ab8a29c72fef
Stephen Lead <stephen.lead@gmail.com>
2015-09-29 20:46:56 +1000

    Add Roles model and controller. Associate with user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,21 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  belongs_to :role
   has_many :videos
   has_many :events
   has_many :upcomings
   has_many :posts
   validates_presence_of :name, :email
+
+  before_save :assign_role
+
+  def assign_role
+    self.role = Role.find_by name: "regular" if self.role.nil?
+  end
+
+  def admin?
+    self.role.name == "admin"
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150927040139) do
+ActiveRecord::Schema.define(version: 20150927120717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,12 @@ ActiveRecord::Schema.define(version: 20150927040139) do
     t.string   "slug",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tags", force: :cascade do |t|
