598e00ff0b96cec2cc351c941766e5d06bdfc990
Stephen Lead <stephen.lead@gmail.com>
2015-10-02 22:14:04 +1000

    remove Admin flag from Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151002120739) do
+ActiveRecord::Schema.define(version: 20151002121337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,7 +100,6 @@ ActiveRecord::Schema.define(version: 20151002120739) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "admin"
     t.string   "name"
     t.integer  "role_id"
   end
