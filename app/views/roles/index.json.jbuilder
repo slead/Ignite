3c15266742fe77c7504713b88c404f2f4b7f08bf
Stephen Lead <stephen.lead@gmail.com>
2015-10-02 22:45:25 +1000

    Add route for Roles. Delete mention of Description in roles views

--- app/views/roles/index.json.jbuilder
+++ app/views/roles/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@roles) do |role|
-  json.extract! role, :id, :name, :description
+  json.extract! role, :id, :name
   json.url role_url(role, format: :json)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   resources :videos
   resources :upcomings
   resources :posts, except: [:index]
+  resources :roles
 
   get '/about', to: 'content#about', as: :about #=> about_path
   get '/admin', to: 'admin#index', as: :admin #=> admin_path
