89cdb15babf3261ff026a1166dbe7fd5aa79c7fa
Stephen Lead <stephen.lead@gmail.com>
2015-10-04 17:09:10 +1100

    Regular users can create videos and upcoming events, and can edit videos for events they own

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,15 +4,26 @@ class Ability
   def initialize(user)
     user ||= User.new # Guest user
     if user.admin?
+      # Admin users have full control
       can :manage, :all
+
     elsif user.regular?
+      # Regular users can create videos and upcoming events, and can edit videos
+      # pertaining to events they own
       can :read, :all
+      can :create, Video
+      can :create, Upcoming
+
       can :update, Event do |event|
         # Users can only edit Events to which they have been granted permission
         event.users.map(&:id).include? user.id
       end
-      can :create, Video
-      can :create, Upcoming
+
+      can :update, Video do |video|
+        # Users can only edit Videos belonging Events to which they have been granted permission
+        video.event.users.map(&:id).include? user.id
+      end
+
     else
       can :read, :all
     end
