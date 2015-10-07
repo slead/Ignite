36e1a8552f9075dc112375130671f9425c6f1cd4
Stephen Lead <stephen.lead@gmail.com>
2015-10-07 20:08:53 +1100

    Remove the password-protection from the Start page

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,5 +1,5 @@
 class ContentController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :except => "start"
   layout 'no_footer', :only => [:start]
   
   def show
