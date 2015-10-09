152be21f1a0d73d9c62dcf364c54086c65cf9279
Stephen Lead <stephen.lead@gmail.com>
2015-10-09 14:44:43 +1100

    Add featured flag to playlists
    so the good ones can be highlighted on the homepage

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008004449) do
+ActiveRecord::Schema.define(version: 20151009034405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20151008004449) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "slug"
+    t.boolean  "featured"
   end
 
   create_table "playlists_events", id: false, force: :cascade do |t|
