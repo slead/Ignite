130490f392df4f5d3e1fa37ef76ee3a2aacc6230
Stephen Lead <stephen.lead@gmail.com>
2015-10-16 10:47:11 +1100

    Show stats on the homepage

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -18,9 +18,10 @@ class ContentController < ApplicationController
     @upcomings = Upcoming.where('date > ?', Date.yesterday).limit(12).order('date ASC')
     @tags = Tag.where(major: true).order(:name)
     @playlists = Playlist.where(featured: true)
-    @total_views = @videos.sum("views")
-    @total_likes = @videos.sum("likes")
-    @total_videos = @videos.count
+    @total_views = Video.all.sum("views")
+    @total_likes = Video.all.sum("likes")
+    @total_videos = Video.all.count
+    @total_events = Event.all.count
 
   end
 
