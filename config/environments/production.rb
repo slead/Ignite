02b127d393b71c45471f14fb265ac5ea87945337
Stephen Lead <stephen.lead@gmail.com>
2015-10-23 19:25:56 +1100

    update action mailer for AWS (was Heroku)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => DOMAIN,
     :enable_starttls_auto => true
   }
 
