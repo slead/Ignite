e37f9cb6a5225afcbd2d3344dfd8ecd793490eb8
Stephen Lead <stephen.lead@gmail.com>
2015-10-24 16:48:31 +1100

    reverting problematic sendgrid code - will sort out later

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => DOMAIN,
+    :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
 
