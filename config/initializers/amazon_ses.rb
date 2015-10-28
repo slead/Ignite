8a895fba2666ce4455c22115690102dd872258d9
Stephen Lead <stephen.lead@gmail.com>
2015-10-28 15:14:21 +1100

    update AWS SES SMTP setting

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,4 +1,5 @@
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
   access_key_id: ENV['AMAZON_ACCESS_KEY'],
   secret_access_key: ENV['SECRET_ACCESS_KEY'],
-  server: 'email.us-west-2.amazonaws.com'
+  server: 'email-smtp.us-west-2.amazonaws.com'
+  
