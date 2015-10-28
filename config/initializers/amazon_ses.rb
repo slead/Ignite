ca2cfdbdbcacb1cc46f33656fd3353ee4315f575
Stephen Lead <stephen.lead@gmail.com>
2015-10-28 14:49:25 +1100

    Change AWS SES email server to US-West-2

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,3 +1,4 @@
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
   access_key_id: ENV['AMAZON_ACCESS_KEY'],
-  secret_access_key: ENV['SECRET_ACCESS_KEY']
+  secret_access_key: ENV['SECRET_ACCESS_KEY'],
+  server: 'email.us-west-2.amazonaws.com'
