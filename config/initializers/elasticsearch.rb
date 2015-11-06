61abe174c516c8f881c852c0dff4b8ef5c2a2d01
Stephen Lead <stephen.lead@gmail.com>
2015-11-06 22:52:35 +1100

    restore elasticsearch after adding initialiser code

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,1 +1,3 @@
 ENV["ELASTICSEARCH_URL"] = "search-ignitetalks-jgbu2tzolcti7hbpxtd6muifm4.us-west-2.es.amazonaws.com"
+
+Searchkick.client = Elasticsearch::Client.new(hosts: ["localhost:9200"], retry_on_failure: true, transport_options: {request: {timeout: 250}})
