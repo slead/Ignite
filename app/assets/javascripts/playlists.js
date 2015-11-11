05244bed251e625012371916829d753be3ce9acd
Stephen Lead <stephen.lead@gmail.com>
2015-11-11 20:31:59 +1100

    Add a button to trawl YouTube for the playlist details
    doesn’t do anything with the results yet

---
+++ app/assets/javascripts/playlists.js
@@ -0,0 +1,45 @@
+var ready;
+ready = function() {
+
+  //Import videos from the YouTube API
+  $("#btnImportFromYouTubePlaylist").on("click", function() {
+    var playlistId = url = $("#txtYouTubePlaylist").val();
+    if(url.indexOf("youtube.com") > 0) {
+      var playlistId = getPlaylistID(url);
+    }    
+    ImportFromYouTubePlaylist(playlistId);
+  });
+
+  function ImportFromYouTubePlaylist(playlistId) {
+    
+    url = "https://www.googleapis.com/youtube/v3/playlistItems?key=AIzaSyD1GKuqhIK7UoPxaLX-PQpCvUlsRYiGD94&part=contentDetails&maxResults=50&fields=items(contentDetails(videoId))&playlistId=" + playlistId
+    $.getJSON( url, function( data ) {
+      if (data !== null && data.items !== null) {
+        for (var idx = 0; idx < data.items.length; idx ++) {
+          try{
+            videoId = data.items[idx].contentDetails.videoId;
+            console.log(videoId);
+          } catch(err){
+            alert(err);
+          }
+        }
+
+      } else {
+        alert("Error - unable to retrieve videos from this YouTube playlist");
+      }
+    });
+  }
+
+  function getPlaylistID(url) {
+
+    var regExp = /^.*(youtu.be\/|list=)([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    if (match && match[2]){
+        return match[2];
+    }
+    return null;
+  }
+}
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
