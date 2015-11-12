2db92d468fd8418b528c4124805ac6ba859d6dfd
Stephen Lead <stephen.lead@gmail.com>
2015-11-12 11:57:41 +1100

    Retrieve each video's thumbnail from a YouTube playlist
    next step is to actually do something with it.

--- app/assets/javascripts/playlists.js
+++ app/assets/javascripts/playlists.js
@@ -3,6 +3,8 @@ ready = function() {
 
   //Import videos from the YouTube API
   $("#btnImportFromYouTubePlaylist").on("click", function() {
+    jQuery("#YouTubeVideos").empty();
+
     var playlistId = url = $("#txtYouTubePlaylist").val();
     if(url.indexOf("youtube.com") > 0) {
       var playlistId = getPlaylistID(url);
@@ -12,21 +14,52 @@ ready = function() {
 
   function ImportFromYouTubePlaylist(playlistId) {
     
-    url = "https://www.googleapis.com/youtube/v3/playlistItems?key=AIzaSyD1GKuqhIK7UoPxaLX-PQpCvUlsRYiGD94&part=contentDetails&maxResults=50&fields=items(contentDetails(videoId))&playlistId=" + playlistId
+    url = "https://www.googleapis.com/youtube/v3/playlistItems?key=AIzaSyD1GKuqhIK7UoPxaLX-PQpCvUlsRYiGD94&part=contentDetails"
+    url += "&maxResults=50&fields=items(contentDetails(videoId))&playlistId=" + playlistId
+
+    // Retrieve the list of playlistItems from the YouTube Playlist
     $.getJSON( url, function( data ) {
       if (data !== null && data.items !== null) {
-        for (var idx = 0; idx < data.items.length; idx ++) {
+        for (var idx = 0; idx < data.items.length; idx ++) {          
           try{
+
+            // Get the uid of each video. Check whether this video already exists in the system
             var videoId = data.items[idx].contentDetails.videoId;
-            var img = "http://img.youtube.com/vi/" + videoId + "/hqdefault.jpg"
-            console.log(img);
+            var videoURL = window.location.origin + "/videos.json" + "?uid=" + videoId;
+            checkIfVideoExists(videoURL, videoId);
+
+            function checkIfVideoExists(videoURL, videoId) {
+              $.ajax ({
+                type: "GET",
+                url: videoURL,
+                success: function(data2) {
+                  exists = false;
+                  if (data2.length > 0) {
+                    exists = true;
+                  }
+
+                  // Add the video's thumbnail to the page. Clicking on the thumbnail will open the
+                  // New Video dialog with this video selected
+                  html = "<div class='playlistItem col-md-2'>"
+                  html += "<span>" + exists + "</span>";
+                  html += "<img src=http://img.youtube.com/vi/" + videoId + "/hqdefault.jpg>"
+                  html += "</div>"
+                  jQuery("#YouTubeVideos").append(html);
+
+                },
+                error: function() {
+                  alert("Sorry, there was a problem importing videos from this playlist");
+                }
+              });
+            }
+
           } catch(err){
-            alert(err);
+            alert("Sorry, there was a problem importing videos from this playlist");
           }
         }
 
       } else {
-        alert("Error - unable to retrieve videos from this YouTube playlist");
+        alert("Sorry - unable to retrieve videos from this YouTube playlist");
       }
     });
   }
