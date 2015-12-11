ca337fc71bceda08c0fb4b402bc04a7460bf80df
Stephen Lead <stephen.lead@gmail.com>
2015-12-12 07:45:34 +1100

    Tweak the Video test by creating an Event outside the loop

--- spec/factories/videos.rb
+++ spec/factories/videos.rb
@@ -1,12 +1,14 @@
 require 'ffaker'
 
 FactoryGirl.define do
+  event = Event.create(name: "Ignite " + FFaker::Address.city, city: FFaker::Address.city, url:FFaker::Internet.http_url)
+
   factory :video do
     title ( FFaker::BaconIpsum.phrase)
     description ( FFaker::BaconIpsum.paragraph )
     uid ( FFaker::Internet.domain_word)
     url ( "http://www.youtube.com/watch?v=" + FFaker::Internet.domain_word)
-    event ( Event.create(name: "Ignite " + FFaker::Address.city, city: FFaker::Address.city, url:FFaker::Internet.http_url))
+    event ( event )
     speaker_name ( FFaker::Name.name )
 
   end
